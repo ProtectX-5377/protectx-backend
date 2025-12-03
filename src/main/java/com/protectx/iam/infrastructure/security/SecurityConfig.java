e47b249c064c4490ca92321557ae2d78dff27e6b
Fabrizio Quiroz <u202213406@upc.edu.pe>
2025-12-03 02:40:29 -0500

    fix: allow all origins in CORS to fix 403 error

--- src/main/java/com/protectx/iam/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/protectx/iam/infrastructure/security/SecurityConfig.java
@@ -47,12 +47,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
         configuration.setAllowedHeaders(List.of("*"));
-
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
